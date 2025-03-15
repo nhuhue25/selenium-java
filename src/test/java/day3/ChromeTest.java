735e37a6a4f429c983139ddd21b482645a5e189c
nguyen nhu hue <239laiguanlin@gmail.com>
2025-03-15 16:34:30 +0700

    second commit

--- src/test/java/day3/ChromeTest.java
+++ src/test/java/day3/ChromeTest.java
@@ -1,26 +1,44 @@
 package day3;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.devtools.DevTools;
+import org.openqa.selenium.devtools.HasDevTools;
+import org.openqa.selenium.devtools.v131.network.Network;
+import org.openqa.selenium.devtools.v133.emulation.Emulation;
 import org.testng.Assert;
 import org.testng.annotations.Test;
-
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
 public class ChromeTest {
     @Test
-    void openInDefaultMode() {
+    void interceptionNetwork(){
         WebDriver driver = new ChromeDriver();
-        driver.get("https://www.selenium.dev/");
-        Assert.assertEquals(driver.getTitle(), "Selenium");
+        DevTools devTool = ((HasDevTools) driver).getDevTools();
+
+        devTool.createSession();
+        devTool.send(Network.enable(Optional.empty(), Optional.empty(), Optional.empty()));
+
+        devTool.addListener(Network.requestWillBeSent(), requestSent -> {
+            System.out.println("Request URL => " + requestSent.getRequest().getUrl());
+            System.out.println("Request Method => " + requestSent.getRequest().getMethod());
+            System.out.println("Request Headers => " + requestSent.getRequest().getHeaders().toString());
+            System.out.println("------------------------------------------------------");
+        });
+
+        devTool.addListener(Network.responseReceived(), responseReceived -> {
+            System.out.println("Response Url => " + responseReceived.getResponse().getUrl());
+            System.out.println("Response Status => " + responseReceived.getResponse().getStatus());
+            System.out.println("Response Headers => " + responseReceived.getResponse().getHeaders().toString());
+            System.out.println("Response MIME Type => " + responseReceived.getResponse().getMimeType().toString());
+            System.out.println("------------------------------------------------------");
+        });
+
+        driver.get("https://selenium.dev");
     }
 
-    @Test
-    void openWithBetaChromeVersion(){
-        ChromeOptions chromeOptions = new ChromeOptions();
-        chromeOptions.setBrowserVersion("136");
-        WebDriver driver = new ChromeDriver (chromeOptions);
-        driver.get("https://www.selenium.dev/");
-        Assert.assertEquals (driver.getTitle(),"Selenium");
-        driver.quit();
-    }
+
 }
